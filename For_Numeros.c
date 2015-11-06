f7ad0d05d8db2898bf2f0f0eaa04622d8d8a9090
Coyotl <alfredocp2509@gmail.com>
2015-11-06 00:51:43 -0600

    Tarea

---
+++ For_Numeros.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/* PRIMER PROGAMA CON FOR*/
+int a;
+
+int main ()
+{//Inicio main
+	printf("Primer programa con For\n");
+	for (a=1; a<=10; a++)
+	printf ("%d\n", a);
+}//Fin main
